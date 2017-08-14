e0dd34083784da2da6ecfd651a27c31b52e01a29
bennpham <bennguyenpham@hotmail.com>
2017-08-13 22:49:00 -0700

    Added Scroll Ability to Navbar

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,8 @@
-
+// JQuery Smooth Scroll Navigation
+$('.smooth').on('click', function () {
+    $.smoothScroll({
+        scrollElement: $('body'),
+        scrollTarget: '#' + this.id
+    });
+    return false;
+});
