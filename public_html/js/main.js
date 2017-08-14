907e2299c373af82cfa7e79b652dedb064ee7713
bennpham <bennguyenpham@hotmail.com>
2017-08-13 23:02:21 -0700

    Replaced smooth scroll

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,8 +1,2 @@
 // JQuery Smooth Scroll Navigation
-$('.smooth').on('click', function () {
-    $.smoothScroll({
-        scrollElement: $('body'),
-        scrollTarget: '#' + this.id
-    });
-    return false;
-});
+var scroll = new SmoothScroll('a[href*="#"]');
